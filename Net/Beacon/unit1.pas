89ac72dd71bb7e637e6ad7c993983d56962530b8
Andrey <dan.soft.studio@gmail.com>
2024-09-26 15:29:59 +0700

    update

---
+++ Net/Beacon/unit1.pas
@@ -0,0 +1,72 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  CommonUtils, NetUtils;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    CheckBox1: TCheckBox;
+    Label1: TLabel;
+    ListBox1: TListBox;
+    Timer1: TTimer;
+    procedure Button1Click(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    var Beacon: TUNet.TBeaconRef;
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  if Timer1.Enabled then Exit;
+  Beacon := TUNet.TBeacon.Create;
+  Timer1.Enabled := True;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if Beacon.Ptr.Enabled then
+  begin
+    Beacon.Ptr.Enabled := False;
+    ListBox1.Clear;
+    Button1.Caption := 'Start';
+  end
+  else
+  begin
+    Beacon.Ptr.Active := CheckBox1.Checked;
+    Beacon.Ptr.Enabled := True;
+    Button1.Caption := 'Stop';
+  end;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+  var Peers: TUInAddrArray;
+  var i: Int32;
+begin
+  Peers := Beacon.Ptr.Peers;
+  ListBox1.Items.BeginUpdate;
+  ListBox1.Items.Clear;
+  for i := 0 to High(Peers) do
+  begin
+    ListBox1.Items.Append(UNetNetAddrToStr(Peers[i]));
+  end;
+  ListBox1.Items.EndUpdate;
+end;
+
+end.
+
