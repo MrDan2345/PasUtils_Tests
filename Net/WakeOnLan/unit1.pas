b282c5e0ad1fd053c4d4f92c0b2c99bfc8855b8b
Andrey <dan.soft.studio@gmail.com>
2024-12-12 11:46:03 +0700

    update

--- Net/WakeOnLan/unit1.pas
+++ Net/WakeOnLan/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, NetUtils;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, NetUtils, Sockets;
 
 type TForm1 = class(TForm)
   Button1: TButton;
@@ -38,21 +38,32 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
   var Sock: TUSocket;
   var Msg: packed record
-    EthHeader: TEthernetHeader;
+    //EthHeader: TEthernetHeader;
     Sync: array[0..5] of UInt8;
     MacAddr: array[0..16 * 6 - 1] of UInt8;
   end;
+  var Addr: TUSockAddr;
+  var Mac: TUMacAddr;
   var i, BytesSent: Int32;
 begin
   //https://en.wikipedia.org/wiki/Wake-on-LAN
-  Msg.EthHeader.SrcAddr := UNetLocalMacAddr;
-  Msg.EthHeader.DestAddr := UNetStrToMacAddr('01:23:45:67:89:AB');
-  Msg.EthHeader.Protocol := htons(ETH_P_ALL);
+  //Msg.EthHeader.SrcAddr := UNetLocalMacAddr;
+  //Msg.EthHeader.DestAddr := UNetStrToMacAddr('18:c0:4d:d8:55:ec');
+  //Msg.EthHeader.Protocol := htons(ETH_P_ALL);
+  Mac := UNetStrToMacAddr('18:c0:4d:d8:55:ec');
   for i := 0 to 5 do Msg.Sync[i] := $ff;
-  for i := 0 to 15 do Move(Msg.EthHeader.DestAddr, Msg.MacAddr[i * 6], SizeOf(TUMacAddr));
-  Sock := TUSocket.Create(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  for i := 0 to 15 do Move(Mac, Msg.MacAddr[i * 6], SizeOf(TUMacAddr));
+  //Sock := TUSocket.Create(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  Sock := TUSocket.CreateUDP();
+  Sock.SetSockOpt(SO_BROADCAST, 1);
   try
-    BytesSent := Sock.SendTo(@Msg, SizeOf(MSg), 0, nil, 0);
+    Addr := TUSockAddr.Default;
+    Addr.sin_addr := TUInAddr.Broadcast;
+    Addr.sin_port := 7;
+    BytesSent := Sock.SendTo(@Msg, SizeOf(MSg), 0, @Addr, SizeOf(Addr));
+    Addr.sin_addr := UNetStrToNetAddr('192.168.1.129');
+    Addr.sin_port := 7;
+    BytesSent := Sock.SendTo(@Msg, SizeOf(MSg), 0, @Addr, SizeOf(Addr));
   finally
     Sock.Close;
   end;
