313dfd61f9f88ae21997a4ead5a0790d5f7fc8c7
Andrey <dan.soft.studio@gmail.com>
2024-12-19 17:57:23 +0700

    update

--- Net/WakeOnLan/unit1.pas
+++ Net/WakeOnLan/unit1.pas
@@ -5,29 +5,42 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, NetUtils, Sockets;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  NetUtils, CommonUtils;
+
+type TPeer = class
+private
+  var _Addr: TUInAddr;
+  var _TimeStamp: UInt64;
+  var _UIPanel: TPanel;
+public
+  property Addr: TUInAddr read _Addr;
+  constructor Create(const AAddr: TUInAddr);
+  destructor Destroy; override;
+  procedure SetupUI(const ParentPanel: TPanel);
+  procedure Update(const NewTimeStamp: UInt64; const Message: String);
+end;
+type TPeerArray = array of TPeer;
 
 type TForm1 = class(TForm)
-  Button1: TButton;
-  Edit1: TEdit;
+  LabelName1: TLabel;
   LabelMac1: TLabel;
   LabelAddress1: TLabel;
-  procedure Button1Click(Sender: TObject);
+  PanelList1: TPanel;
+  Timer1: TTimer;
+  procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
   procedure FormCreate(Sender: TObject);
 private
+  var LocalName: String;
   var LocalAddr: TUInAddr;
   var LocalMac: TUMacAddr;
+  var Beacon: TUNet.TBeaconRef;
+  var Peers: TPeerArray;
 public
-
+  function FindPeer(const Addr: TUInAddr): TPeer;
+  function FindOrAddPeer(const PeerInfo: TUNet.TBeacon.TPeer): TPeer;
+  procedure OnTimer(Sender: TObject);
 end;
-
-type TEthernetHeader = packed record
-  DestAddr: TUMacAddr;
-  SrcAddr: TUMacAddr;
-  Protocol: UInt16;
-end;
-
-const ETH_P_ALL = $0003; // Protocol for capturing all packets
 
 var Form1: TForm1;
 
@@ -35,52 +48,102 @@ implementation
 
 {$R *.lfm}
 
-procedure TForm1.Button1Click(Sender: TObject);
-  var Sock: TUSocket;
-  var Msg: packed record
-    //EthHeader: TEthernetHeader;
-    Sync: array[0..5] of UInt8;
-    MacAddr: array[0..16] of TUMacAddr;
-  end;
-  var Addr: TUSockAddr;
-  var Mac: TUMacAddr;
-  var i, BytesSent: Int32;
+constructor TPeer.Create(const AAddr: TUInAddr);
 begin
-  //https://en.wikipedia.org/wiki/Wake-on-LAN
-  //Msg.EthHeader.SrcAddr := UNetLocalMacAddr;
-  //Msg.EthHeader.DestAddr := UNetStrToMacAddr('18:c0:4d:d8:55:ec');
-  //Msg.EthHeader.Protocol := htons(ETH_P_ALL);
-  Mac := UNetStrToMacAddr('18:c0:4d:d8:55:ec');
-  for i := 0 to 5 do Msg.Sync[i] := $ff;
-  for i := 0 to High(Msg.MacAddr) do Msg.MacAddr[i] := Mac;
-  //Sock := TUSocket.Create(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-  Sock := TUSocket.CreateUDP();
-  Sock.SetSockOpt(SO_BROADCAST, 1);
-  try
-    Addr := TUSockAddr.Default;
-    Addr.sin_addr := UNetLocalAddr;// TUInAddr.Broadcast;
-    Addr.sin_addr.Addr8[3] := $ff;
-    Addr.sin_port := UNetHostToNetShort(7);
-    for i := 0 to 4 do
-    begin
-      BytesSent := Sock.SendTo(@Msg, SizeOf(Msg), 0, @Addr, SizeOf(Addr));
-      Addr.sin_addr := UNetStrToNetAddr('192.168.1.129');
-      BytesSent := Sock.SendTo(@Msg, SizeOf(Msg), 0, @Addr, SizeOf(Addr));
-      Sleep(10);
-    end;
-  finally
-    Sock.Close;
+  _Addr := AAddr;
+  _TimeStamp := 0;
+  _UIPanel := nil;
+end;
+
+destructor TPeer.Destroy;
+begin
+  FreeAndNil(_UIPanel);
+  inherited Destroy;
+end;
+
+procedure TPeer.SetupUI(const ParentPanel: TPanel);
+begin
+  _UIPanel := TPanel.Create(ParentPanel);
+  _UIPanel.Parent := ParentPanel;
+  with _UIPanel do
+  begin
+    Height := 32;
+    Color := $808080;
+    BevelOuter := bvSpace;
+    BevelColor := $c0c0c0;
+    BevelWidth := 2;
+    BorderStyle := bsNone;
+    Align := alTop;
+    BorderSpacing.Around := 4;
   end;
+end;
+
+procedure TPeer.Update(const NewTimeStamp: UInt64; const Message: String);
+begin
+  if _TimeStamp = NewTimeStamp then Exit;
+  _TimeStamp := NewTimeStamp;
+
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  LocalName := UNetHostName;
   LocalAddr := UNetLocalAddr;
   LocalMac := UNetLocalMacAddr;
-  LabelAddress1.Caption := 'Address: ' + UNetNetAddrToStr(LocalAddr);
-  LabelMac1.Caption := 'Mac: ' + UNetMacAddrToStr(LocalMac);
-  Edit1.Text := UNetMacAddrToStr(LocalMac);
+  LabelName1.Caption := LocalName;
+  LabelAddress1.Caption := UNetNetAddrToStr(LocalAddr);
+  LabelMac1.Caption := UNetMacAddrToStr(LocalMac);
   Caption := 'Wake On Lan ' + UNetNetAddrToStr(LocalAddr) + ' ' + UNetMacAddrToStr(LocalMac);
+  Beacon := TUNet.TBeacon.Create;
+  Beacon.Ptr.Active := True;
+  Beacon.Ptr.BroadcastInterval := 30 * 1000;
+  Beacon.Ptr.Message := UNetMacAddrToStr(UNetLocalMacAddr);
+  Beacon.Ptr.Port := 57210 + 22;
+  Beacon.Ptr.Enabled := True;
+  Timer1 := TTimer.Create(Self);
+  Timer1.Interval := 1000;
+  Timer1.OnTimer := @OnTimer;
+  Timer1.Enabled := True;
+  //AddPeer;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  specialize UArrClear<TPeer>(Peers);
+end;
+
+function TForm1.FindPeer(const Addr: TUInAddr): TPeer;
+  var i: Int32;
+begin
+  for i := 0 to High(Peers) do
+  if Peers[i].Addr = Addr then Exit(Peers[i]);
+  Result := nil;
+end;
+
+function TForm1.FindOrAddPeer(const PeerInfo: TUNet.TBeacon.TPeer): TPeer;
+  var Peer: TPeer;
+begin
+  Peer := FindPeer(PeerInfo.Addr);
+  if not Assigned(Peer) then
+  begin
+    Peer := TPeer.Create(PeerInfo.Addr);
+    Peer.SetupUI(PanelList1);
+    specialize UArrAppend<TPeer>(Peers, Peer);
+  end;
+  Peer.Update(PeerInfo.TimeStamp, PeerInfo.Message);
+end;
+
+procedure TForm1.OnTimer(Sender: TObject);
+  var BeaconPeers: TUNet.TBeacon.TPeerArray;
+  var Peer: TPeer;
+  var i: Int32;
+begin
+  BeaconPeers := Beacon.Ptr.Peers;
+  for i := 0 to High(BeaconPeers) do
+  begin
+    Peer := FindOrAddPeer(BeaconPeers[i]);
+    Peer.Update(BeaconPeers[i].TimeStamp, BeaconPeers[i].Message);
+  end;
 end;
 
 end.
